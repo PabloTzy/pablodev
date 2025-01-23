e95dacf6a402361c52856a879c4d1b4dd64f27a2
PabloTzy <135358752+PabloTzy@users.noreply.github.com>
2025-01-23 15:22:29 +0700

    Create nebula-installer.sh

---
+++ nebula-installer.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Banner
+echo "==============================================="
+echo "           CHECK BLUEPRINT FILES               "
+echo "==============================================="
+
+# Check if Blueprint is installed
+BLUEPRINT_FILE="/var/www/pterodactyl/blueprint.sh"
+if [ ! -f "$BLUEPRINT_FILE" ]; then
+    echo "==============================================="
+    echo "   BLUEPRINT BELUM DIINSTALL SILAKAN MENGINSTALL BLUEPRINT TERLEBIH DAHULU"
+    echo "==============================================="
+    exit 1
+fi
+
+# Installing Nebula
+echo "==============================================="
+echo "           INSTALLING THEME NEBULA             "
+echo "==============================================="
+
+NEBULA_URL="https://pablotzy.github.io/pablodev/nebulaptero.zip"
+cd /var/www
+
+echo "Downloading Nebula theme..."
+wget "$NEBULA_URL" -O nebulaptero.zip
+
+echo "Extracting Nebula theme..."
+unzip -o nebulaptero.zip -d /var/www/
+
+echo "Applying Nebula theme using Blueprint..."
+cd /var/www/pterodactyl && bash blueprint.sh -install nebula
+
+echo "Cleaning up temporary files..."
+rm -f /var/www/nebulaptero.zip
+cd /var/www/pterodactyl && rm -f nebula.blueprint
+
+# Success message
+echo "==============================================="
+echo "   SUCCESSFULLY INSTALLED THEME NEBULA         "
+echo "   SCRIPT INSTALLER BY PABLO STORE             "
+echo "==============================================="
